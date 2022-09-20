f0bce18a751623f3439c828e9a02746f02e2a8d9
Vitor Wagner <vitormwagner@gmail.com>
2022-09-20 15:51:53 -0300

    Requisito 7

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,15 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+// referência para arrayGenerator: https://stackoverflow.com/questions/38824349/how-to-convert-an-object-to-an-array-of-key-value-pairs-in-javascript
 
-const arrayGenerator = (type, object) => {};
+const calculator = (number1, number2) => ({
+  sum: number1 + number2,
+  mult: number1 * number2,
+  div: Math.floor(number1 / number2),
+  sub: number1 - number2,
+});
+
+const arrayGenerator = (type, object) => Object[type](object);
 
 module.exports = { calculator, arrayGenerator };
