40f3e89237fc99ef4eb21ead12cf74bd7f17d3fb
Vitor Wagner <vitormwagner@gmail.com>
2022-09-20 15:12:22 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -11,6 +11,14 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  if (array.length === 0) return undefined;
+  let sum = 0;
+  for (const number of array) {
+    if (typeof number !== 'number') return undefined;
+    sum += number;
+  }
+  return Math.round(sum / array.length);
+};
 
 module.exports = average;
